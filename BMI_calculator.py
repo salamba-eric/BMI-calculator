28721cc611031bf26f43a7844a5ddffbc0231cd0
eric-sala <ericsalamba@gmail.com>
2021-05-29 22:59:01 +0300

    focus db

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,8 +1,15 @@
 from tkinter import *
+from pymongo import *
+
 
 window = Tk()
-window_sign_in = Tk()
+client = MongoClient()
+sign = sign_up()
 
+Data_Base = client.BMI
+users_data = Data_Base.users_data
+
+#def calculate_bmi():
 	# Calculating function #
 def metric_calculator():
 		# Clear what's on the space #
@@ -13,17 +20,35 @@ def metric_calculator():
 		weight = int(weight_str.get())
 		BMI = weight/(height*height)
 	except ValueError:
-		print("You have to input a value for both height and weight")
+		value_error()
 		BMI = 0
-		# Actual calculator #
 	
 		# What to display #
 	if BMI != 0:
-		BMI_listbox.insert(END , ">" + str(BMI))
-		BMI_display_entry.insert(END , str(BMI))
+		BMI_listbox.delete(0, 'end')
+		BMI_display_entry.insert(END ,str(BMI))
+		Error_msg1.destroy()
+		if BMI <16:
+			comment = "You are Severely Underweight"
+		elif BMI < 18:
+			comment = "You are Underweight"
+		elif BMI < 23:
+			comment = "You are Healthy"
+		elif BMI <29:
+			comment = "You are Overweight"
+		elif BMI <39:
+			comment = "You are Obese"
+		else:
+			comment = "You are Extremely Obese"
 
+	BMI_listbox.insert(1, "    " + str(BMI))
+	BMI_listbox.insert(2 ,comment)
 
-# Window widgets #
+def edit():
+	for doc1 in users_data.find():
+		users_data.update_one({"Username":"Eric Salamba"},{"$set":{"BMI":"female"}})
+
+					# Window widgets #
 	# The labels #
 label_1 = Label(window, text = "This will calculate your BMI", fg = "black", font = ("Arial" , 12))
 label_2 = Label(window, text = "Input your weight and height below", fg = "black", font = ("Arial" , 12))
@@ -31,16 +56,17 @@ height_label = Label(window, text = "Enter your height (cm)", fg = "black", font
 weight_label = Label(window, text = "Enter your weight (kg)", fg = "black", font = ("Arial" , 14))
 BMI_display_label = Label(window, text = "Your BMI is ", fg = "black", font = ("Arial", 14))
 list_label = Label(window, text = "Your previous BMI's", font = ("Arial" , 12))
+Error_msg1 = Label(window, text = "Input numerals as your height and weight", fg = "red", font = ("Arial", 12))
 	#The entrys #
 height_str = Entry(window, bg = "white", fg = "black", bd = 3)
 weight_str = Entry(window, bg = "white", fg = "black", bd = 3)
 BMI_display_entry = Entry(window, font = ("Arial", 12))
 	#The buttons#
-calculate_button = Button(window,text = "Calculate", bd = 1, font = ("Arial" , 12), command = metric_calculator)
+calculate_button = Button(window,text = "Calculate", bd = 1, font = ("Arial" , 12), command = edit)
 	#The listbox #
 BMI_listbox = Listbox(window,width = 30, height = 11, font = ("Raleway" ))
 
-	#Widget placements #
+					#Widget placements #
 		# labels #
 label_1.place(x = 75, y = 25)
 label_2.place(x = 50, y = 50)
@@ -52,13 +78,21 @@ list_label.place(x = 100, y = 265)
 height_str.place(x = 200, y = 90 ,width = 90 , height = 20)
 weight_str.place(x = 200, y = 118 ,width = 90 , height = 20)
 BMI_display_entry.place(x = 90, y = 210)
+
 		# buttons #
 calculate_button.place(x = 125, y = 150)
 		# listbox #
-BMI_listbox.place(x = 90, y = 285,width = 190, height = 200)
+BMI_listbox.place(x = 25, y = 285,width = 300, height = 120)
+
+		# Error handling #
+def value_error():
+	Error_msg1.place(x = 33, y = 240)
 
   
    #Generating the window #  
 window.title("BMI Calculator")
 window.geometry("350x500+550+100")
 window.mainloop()
+
+
+
--- sign_up.py
+++ sign_up.py
@@ -1,13 +1,64 @@
 from tkinter import *
 from pymongo import *
+# from BMI_calculator import calculate_bmi
+
+
 window_sign_in = Tk() 
 client = MongoClient()
+sex = StringVar()
 
 Data_Base = client.BMI
 users_data = Data_Base.users_data
 
-profile = {"user_name" : "Eric" , "gender" : "male" , "Body_mass_Idex" : 20}
-user1 = users_data.insert_one(profile)
+
+		
+		# functions #
+def query_info():
+
+	user_name = user_entry.get()
+	return user_name
+
+def signing_in():
+
+	user_name = user_entry.get()
+	gender = str(sex.get())
+	profile = { "Username" : user_name , "Gender" : gender}
+
+	users_data.insert_one(profile)
+
+	window_sign_in.destroy()
+	calculate_bmi()
+
+
+def finish():
+	sign_in_button.place(x = 100, y = 165 , width = 100 , height = 25)
+
+		# Widgets #
+	# Labes #
+label_1 = Label(window_sign_in, text = " Sign in ", font = ("Arial" , 17))
+user_label = Label(window_sign_in, text = "Username :", font = ("Arial" , 15))
+password_label = Label(window_sign_in, text = "Password  :" , font = ("Arial", 15))
+	# Entrys #
+user_entry = Entry(window_sign_in, bd = 3)
+password_entry = Entry(window_sign_in, bd = 3)
+	# Buttons #
+sign_in_button = Button(window_sign_in, text = " Done ", command = signing_in)
+	# Radiobutton #
+male_button = Radiobutton(window_sign_in, text = "male", borderwidth = 0, variable = sex, value = "male", font = ("Arial" , 12), command = finish)
+female_button = Radiobutton(window_sign_in, text = "female", borderwidth = 0, variable = sex, value = "female", font = ("Arial" , 12), command = finish)
+
+
+		# Widget placement #
+			# Labels #
+label_1.place(x = 90 , y = 20)
+user_label.place(x = 5 , y = 65)
+password_label.place(x = 2, y = 90)
+			# Entry #
+user_entry.place(x = 115 , y = 70 , width = 175)
+password_entry.place(x = 115, y = 95 , width = 175)
+			#Radiobutton #
+male_button.place(x = 65 , y = 130)
+female_button.place(x = 175 , y = 130)
 
 
 
@@ -15,6 +66,6 @@ user1 = users_data.insert_one(profile)
 
 
 	# A sign in window #
-# window_sign_in.title("Sign in")
-# window_sign_in.geometry("350x350+570+100")
-# window_sign_in.mainloop()
+window_sign_in.title("Sign in")
+window_sign_in.geometry("300x200+570+100")
+window_sign_in.mainloop()
